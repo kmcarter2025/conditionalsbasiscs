e4daad99b0fa1ad3d6c596f178bb87ccde0195bf
Kim <87247181+kmcarter2025@users.noreply.github.com>
2023-06-25 10:19:01 -0700

    Create main6.py
    Numeric comparison

---
+++ main6.py
@@ -0,0 +1,14 @@
+# numeric comparison
+a = 5
+
+if a > 0:
+    print('greater than 0')
+
+if a >= 0:
+    print('greater than or equal to 0')
+
+if a != 0:
+    print('not equal to 0')
+
+if a == 0:
+    print('equal to 0')
