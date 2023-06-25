f94a4a77d0a025206652e1ea280e0e577610b1ee
Kim <87247181+kmcarter2025@users.noreply.github.com>
2023-06-25 13:56:12 -0700

    Create main10.py
    method basics

---
+++ main10.py
@@ -0,0 +1,7 @@
+var = 'Hello, World'
+var2 = var.replace('World', 'Mars')   # replace substring, return a str
+print(var2)                           # Hello, Mars
+
+var = 'Hello, World!'
+var2 = var.count('l')                 # Count the 'l' characters return an int
+print(var2)
