bc941efd61b3e76997692d0aac7d8c5e8e24c8d9
Kim <87247181+kmcarter2025@users.noreply.github.com>
2023-06-25 15:37:55 -0700

    Create inspecting1.py
    inspecting

---
+++ inspecting1.py
@@ -0,0 +1,10 @@
+mystring = '12345'
+if mystring.isdigit():
+    print("that string is all numeric characters")
+
+if not mystring.isdigit():
+    print(" that string is not all numeric characters")
+
+x = input ('please enter a int" ')
+if x.isdigit():
+    y = int(x)
