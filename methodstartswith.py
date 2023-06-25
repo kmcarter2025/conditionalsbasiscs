3febd8edfc493bf430115ce2b3e92ca12ea71053
Kim <87247181+kmcarter2025@users.noreply.github.com>
2023-06-25 15:47:08 -0700

    Create methodstartswith.py
    starts with

---
+++ methodstartswith.py
@@ -0,0 +1,7 @@
+# startswith(): True if this string starts with arg string
+
+cc = input('yes? ')
+if cc.startswith('y') or cc.startswith('Y'):
+    print('thanks!')
+else:
+    print("ok, I guess not.")
